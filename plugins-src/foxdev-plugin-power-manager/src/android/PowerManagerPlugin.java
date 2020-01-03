57b27c844d540b5bf1b444bf14033daa67794a18
Peter Fox <peter@foxdev.co.uk>
2020-01-03 01:08:46 +0000

    Power Manager Plugin : Working 'wakeDevice' code!

--- plugins-src/foxdev-plugin-power-manager/src/android/PowerManagerPlugin.java
+++ plugins-src/foxdev-plugin-power-manager/src/android/PowerManagerPlugin.java
@@ -10,6 +10,17 @@ import org.json.JSONObject;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+import android.content.Context;
+import android.os.PowerManager;
+
+import android.view.WindowManager;
+
+import android.app.Activity;
+import android.util.Log;
+import android.os.PowerManager.WakeLock;
+import java.util.concurrent.TimeUnit;
+
+
 /**
 * This class echoes a string called from JavaScript.
 */
@@ -26,7 +37,7 @@ public class PowerManagerPlugin extends CordovaPlugin {
 	
 	private void wakeDevice(JSONArray args, CallbackContext callbackContext) {
 
-		//this.wakeDevice();
+		this.wakeDevice();
 
 		AlertDialog.Builder builder1 = new AlertDialog.Builder(this.cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
 		builder1.setMessage("Write your message here.");
@@ -63,22 +74,38 @@ public class PowerManagerPlugin extends CordovaPlugin {
 	}
 
 	public void wakeDevice() {
-//		PowerManager powerManager = (PowerManager) getSystemService(this.cordova.getActivity().POWER_SERVICE);
-//		wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-//		wakeLock.acquire();
-//
-//		KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
-//		KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
-//		keyguardLock.disableKeyguard();
-//		runOnUiThread(new Runnable(){
-//			public void run(){
-//				getWindow().addFlags(
-//						WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-//								| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-//								| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-//								| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-//			}
-//		});
+		
+		this.turnScreenOnThroughKeyguard(cordova.getActivity());
+		
 	}
+	
+	/**
+	 * Attempt a few methods of waking the screen!
+	 * https://stackoverflow.com/a/52403450
+	 */
+	public void turnScreenOnThroughKeyguard(Activity activity) {
+		this.userPowerManagerWakeup(activity);
+		//this.useWindowFlags(activity);	// Throws exception.
+		this.useActivityScreenMethods(activity);
+	}
+
+	private void useActivityScreenMethods(Activity activity) {
+		activity.setTurnScreenOn(true);
+		activity.setShowWhenLocked(true);
+	}
+
+	private void useWindowFlags(Activity activity) {
+		activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+				| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+				| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+				| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+	}
+
+	private void userPowerManagerWakeup(Activity activity) {
+		PowerManager pm = (PowerManager) this.cordova.getActivity().getApplicationContext().getSystemService(Context.POWER_SERVICE);
+		WakeLock wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "myapp:taggy");
+		wakeLock.acquire(TimeUnit.SECONDS.toMillis(5));
+	}
+
 
 }
