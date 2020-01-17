deb73ffa117c006527bd131116c621f75c93a81b
Peter Fox <peter@foxdev.co.uk>
2020-01-17 23:28:54 +0000

    Removed unused custom power plugin

--- plugins-src/foxdev-plugin-power-manager/www/powerManager.js
+++
@@ -1,7 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.wakeDevice = function (success, error) {
-	if (!success) { success = function() {}; }
-	if (!error) { error = function() {}; }
-    exec(success, error, 'PowerManagerPlugin', 'wakeDevice', []);
-};
